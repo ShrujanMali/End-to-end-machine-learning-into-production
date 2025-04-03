598be1dd8a40c73a450341c569b33af3c52ba969
shrujanmali24@gmail.com <shrujanmali24@gmail.com>
2025-04-03 09:09:08 +0530

    final commit

---
+++ test.py
@@ -0,0 +1,14 @@
+from src.logger.logging import logging
+
+logging.info("this is my testing")
+
+logging.info("this is my second testing")
+logging.info("this is my second testing")
+
+logging.info("this is my second testing")
+
+logging.info("this is my second testing")
+
+logging.info("this is my second testing")
+
+logging.info("this is my second testing")
